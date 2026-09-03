c9bd7bb90c03e633caf6992a0c7cf5527f60cd02
Fernando Rodriguez <rodriguezfernandohernan_github@gmail.com>
2026-09-03 20:25:41 -0300

    Fix: Configurar HOST 127.0.0.1 en servidor express

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '127.0.0.1';
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`===========================================`);
-  console.log(` Servidor corriendo en http://localhost:${PORT}`);
+  console.log(` Servidor corriendo en http://${HOST}:${PORT}`);
   console.log(`===========================================`);
 });
