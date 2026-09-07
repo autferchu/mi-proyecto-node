f47240df7428063a7a1eedbff45ab84fd75f3b2e
Fernando Rodriguez <rodriguezfernandohernan_github@gmail.com>
2026-09-07 20:25:11 -0300

    Preparar servidor para despliegue

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const app = require('./src/app');
+const app = require("./src/app");
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '127.0.0.1';
+const HOST = "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
-  console.log(`===========================================`);
-  console.log(` Servidor corriendo en http://${HOST}:${PORT}`);
-  console.log(`===========================================`);
+  console.log("===========================================");
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log("===========================================");
 });
